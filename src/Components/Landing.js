e98d680b0136eb6d47b43e1b2209c02a16a05863
Carson Davies <carsond11@gmail.com>
2021-03-03 15:06:31 -0700

    contact section added and styled with media queries

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -5,6 +5,21 @@ const Landing = () => {
 
   return (
     <div className='landing'>
+      <div className='contact'>
+        <div className='contact-left-side'>
+          <p className='contact-statement'>Utilization Management (UM) staff are available from 8:00am - 5:00pm Monday through Friday to answer Member questions related to the authorization and referral processes. After-Hour nurse is available after 5:00pm Monday through Friday, Weekends and Holidays for urgent issues. You may also request a copy of the criteria used in the decision making process. UM staff are bilingual in Spanish or we can help set up an interpreter for you.</p>
+        </div>
+        <div className='contact-right-side'>
+          <p className='contact-title'>Utilization Management</p>
+          <p className='contact-info'>Direct line: (562) 206-1360</p>
+          <p className='contact-info'>Toll-free: (562) 961-3022</p>
+          <p className='contact-title'>Via Fax</p>
+          <p className='contact-info'>(562) 206-1360</p>
+          <p className='contact-title'>Via TDD/TYY</p>
+          <p className='contact-info'>Call the TDD/TYY # on your Member ID card or dial 711 for Telecommunications Relay Service</p>
+        </div>
+
+      </div>
 
       <div className='aff-box'>
         <p className='aff-title'>AFFIRMATION STATEMENT</p>
@@ -20,6 +35,8 @@ const Landing = () => {
           decisions that will not influence hiring, compensation, termination, promotion or any other similar matters.
           </p>
       </div>
+
+
 
       <div className='claims-box'>
         <div className='claims'>
